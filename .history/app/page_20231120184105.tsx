81810382f759054011f6d73b2eeffe8492929e82
BrianPhamDev <phamhoanglinh1998@gmail.com>
2023-11-20 18:52:23 -0800

    Setting & style for Tailwind, Eslint, and Prettier

--- .history/app/page_20231120184105.tsx
+++ .history/app/page_20231120184105.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1 className="h1-bold">Heading</h1>
+      <h1>Heading</h1>
     </div>
   );
 }
