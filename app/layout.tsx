7469ee3cad3fdccc8efb48a1dc852369de5055fa
BrianPhamDev <phamhoanglinh1998@gmail.com>
2023-11-20 20:32:21 -0800

    Implemented ThemeProvider context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs"; // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 import "./globals.css";
 
@@ -41,7 +42,7 @@ export default function RootLayout({
             },
           }}
         >
-          {children}
+          <ThemeProvider>{children}</ThemeProvider>
         </ClerkProvider>
       </body>
     </html>
