375cf8d124f25697127e4f33c1c96b8aaad2c657
BrianPhamDev <phamhoanglinh1998@gmail.com>
2023-11-20 23:16:54 -0800

    deploy test

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -48,7 +48,7 @@ export async function POST(req: Request) {
       "svix-signature": svixSignature,
     }) as WebhookEvent;
   } catch (err) {
-    console.error("Error verifying webhook:", err);
+    console.error("Error verifying webhook: ", err);
     return new Response("Error occured", {
       status: 400,
     });
