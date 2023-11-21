474db467fed8ae34e3520da22a70719d17f21067
BrianPhamDev <phamhoanglinh1998@gmail.com>
2023-11-20 19:14:20 -0800

    Create routing to ask-question and auth

---
+++ .history/app/(auth)/sign-in/page_20231120190615.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = ({ children }: { children: React.ReactNode }) => {
+  return <div>Sign-in</div>;
+};
+
+export default page;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import page from "../sign-in/page";
+
+const SignUp = () => {
+  return <div>Sign Up</div>;
+};
+
+export default page;
