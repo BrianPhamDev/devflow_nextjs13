a50e416bb3768b665cd12d83f923f3efc92cf759
BrianPhamDev <phamhoanglinh1998@gmail.com>
2023-11-23 22:05:35 -0800

    Implemented Community page

---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,48 @@
+// "use client";
+
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import UserCard from "@/components/shared/cards/UserCard";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { UserFilters } from "@/constants/filters";
+import { getAllUsers } from "@/lib/actions/user.action";
+import React from "react";
+
+const CommunityPage = async () => {
+  const result = await getAllUsers({});
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/community"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for amazing minds"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={UserFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result.length > 0 ? (
+          result.map((user) => <UserCard key={user._id} user={user}></UserCard>)
+        ) : (
+          <NoResult
+            title="No Users Found"
+            description="Be the first to break the silence! 🚀 Signup to be the first and kickstart the community. Get involved! 💡"
+            link="/sign-up"
+            linkTitle="Sign Up"
+          />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default CommunityPage;
